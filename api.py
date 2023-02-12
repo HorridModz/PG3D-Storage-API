21fbdfa2b7977f6c335cfcefc8e8a8761faf5b12
HorridModz <105762560+HorridModz@users.noreply.github.com>
2023-02-12 14:50:56 -0500

    Update api.py
    Almost a complete rewrite. Lots of improvements:
    
    -Refactoring and reformatting
    -Better error handling
    -More methods to import requests module
    -Ability to automatically update
    -Ability to disable update checks, or just ignore update checks for a specific version
    -Detects whether it is being run on android (to make it easier to diagnose problems)
    -Checks if Pixel Gun 3D is installed
    -Automatically runs and prints results if executed directly, but not if imported as a module
    
    I tested everything on my Windows 11 PC, but I did not test on android. For example, the android detection is untested.
    I also credited myself as one of the authors and changed the version to 2.0.0. Feel free to change this if you don't like it.

--- api.py
+++ api.py
@@ -1,47 +1,218 @@
-__author__ = "ChrxnZX | Pulsed#1874"
-__version__ = "1.1.2"
-
-initialSetupIndex = 0
+__author__ = "ChrxnZX | Pulsed#1874 + HorridModz | User123456789#6424"
+__version__ = "2.0.0"
+ignoreversion = "2.0.0"
+updatechecking = True
 
 import os
+import sys
+import re
+
+_importsuccess = False
+# Method 1
 try:
-	import requests
-	#from requests import NewConnectionError
+    import requests
 except ModuleNotFoundError:
-	initialSetupIndex += 1
-	os.system("pip install requests")
-	print("Initial Setup Index No. " + str(initialSetupIndex))
-	import requests
+    _importsuccess = False
+else:
+    _importsuccess = True
+# Method 2
+if not _importsuccess:
+    try:
+        import pip
+        pip.main(["install", "requests"])
+        import requests
+    except Exception:
+        _importsuccess = False
+    else:
+        _importsuccess = True
+# Method 3
+if not _importsuccess:
+    try:
+        import subprocess
+        subprocess.call("pip install requests")
+        import requests
+    except Exception:
+        _importsuccess = False
+    else:
+        _importsuccess = True
+# Method 4
+if not _importsuccess:
+    try:
+        import subprocess
+        subprocess.call([sys.executable, "-m", "pip", "install", "requests"])
+        import requests
+    except Exception:
+        _importsuccess = False
+    else:
+        _importsuccess = True
+del _importsuccess
 
 
+def _getrequest(link, errormessage=None):
+    if "requests" in globals():
+        try:
+            response = requests.get(link)
+        except requests.exceptions.ConnectionError:
+            if errormessage is not None:
+                print(f"{errormessage}: No internet connection.")
+        except requests.exceptions.HTTPError as err:
+            if errormessage is not None:
+                print(f"{errormessage}: HTTP error occurred during request to {link}: {str(err)}.")
+        except requests.exceptions.Timeout as err:
+            if errormessage is not None:
+                print(f"{errormessage}: Request to {link} timed Out: {str(err)}.")
+        except Exception as err:
+            if errormessage is not None:
+                print(f"{errormessage}: Request to {link} failed: {str(err)}.")
+    else:
+        if errormessage is not None:
+            print(f"{errormessage}: Failed to import requests module.")
+    if "response" in locals() and response.status_code != 200:
+        if errormessage is not None:
+            print(f"{errormessage}:"
+                  f" {link}"
+                  " returned http status code"
+                  f" {str(response.status_code)}")
+    return (response.status_code == 200, response.text) if "response" in locals() else (None, None)
 
-try:
-    if not(requests.get("https://raw.githubusercontent.com/ChrxnZ/PG3D-Storage-API/main/Info/currentVer.txt").text.rstrip("\n") == vars()["__version__"]):
-    	print("This API has an Update! https://github.com/ChrxnZ/PG3D-Storage-API/")
-except requests.exceptions.ConnectionError:
-    print("Unable To Check For Updates: No Internet Connection. Ignoring..")
-except requests.exceptions.HTTPError as err:
-    print("Unable To Check For Updates: HTTP Error Occured:" + str(err) + ". Ignoring..") 
-except requests.exceptions.Timeout as err:
-    print("Unable To Check For Updates: Request Timed Out:" + str(err) + ". Ignoring..")
-except requests.exceptions.RequestException as err:
-    print("Unable To Check For Updates: Request Failed:" + str(err) + ". Ignoring..")
+
+def checkforupdates():
+    def _ignoreversion(version):
+        with open(__file__, 'w') as f:
+            new = re.sub("\nignoreversion = .*\n", f"\nignoreversion = \"{version}\"\n", currentcode)
+            f.write(new)
+
+    def _update():
+        success, text = _getrequest("https://raw.githubusercontent.com/ChrxnZ/PG3D-Storage-API/main/api.py",
+                                    errormessage=None)
+        if success:
+            with open(__file__, 'w') as f:
+                f.write(text)
+        else:
+            raise requests.exceptions.RequestException(
+                "Request to https://raw.githubusercontent.com/ChrxnZ/PG3D-Storage-API/main/api.py failed.")
+
+    # End of _update() function
+    success, text = _getrequest("https://raw.githubusercontent.com/ChrxnZ/PG3D-Storage-API/main/Info/currentVer.txt",
+                                errormessage="PG3D API: Unable to check for updates")
+    if success:
+        cloudversion = text.strip()
+        if not (cloudversion == __version__ or cloudversion == ignoreversion):
+            print("PG3D API has an update! https://github.com/ChrxnZ/PG3D-Storage-API/")
+            toupdate = input("Would you like to update it? (Y/N):")
+            if toupdate.strip().lower() == "y":
+                with open(__file__, 'r') as f:
+                    currentcode = f.read()
+                try:
+                    _update()
+                except Exception as err:
+                    print("Update failed: " + str(err))
+                    try:
+                        with open(__file__, 'r+') as f:
+                            if f.read() != currentcode:
+                                print("PG3D API has been corrupted. Attempting to repair...")
+                                try:
+                                    f.write(currentcode)
+                                except Exception:
+                                    print("Failed to repair PG3D API.")
+                                else:
+                                    print("Successfully repaired PG3D API.")
+                    except Exception:
+                        pass
+                else:
+                    print("Successfully updated PG3D API! Rerun the api.")
+                    sys.exit()
+            else:
+                toignore = input("Would you like to permanently ignore this version? (Y/N):")
+                if toignore.strip().lower() == "y":
+                    with open(__file__, 'r') as f:
+                        currentcode = f.read()
+                    try:
+                        _ignoreversion(cloudversion)
+                    except Exception as err:
+                        print("Failed to permanently ignore this version: " + str(err))
+                        try:
+                            with open(__file__, 'r+') as f:
+                                if f.read() != currentcode:
+                                    print("PG3D API has been corrupted. Attempting to repair...")
+                                    try:
+                                        f.write(currentcode)
+                                    except Exception:
+                                        print("Failed to repair PG3D API.")
+                                    else:
+                                        print("Successfully repaired PG3D API.")
+                        except Exception:
+                            pass
+                    else:
+                        print("Successfully ignored this version! Rerun PG3D API.")
+                        sys.exit()
+        else:
+            print("No updates found")
 
 
-
-Paths = {
-	"LBTXS" : "/storage/emulated/0/Android/data/com.pixel.gun3d/files/lobby_textures/"
+_Paths = {
+    "PG3DRoot": "/storage/emulated/0/Android/data/com.pixel.gun3d",
+    "LBTXS": "/storage/emulated/0/Android/data/com.pixel.gun3d/files/lobby_textures/",
 }
 
-def getFolderNamesInDIR(Dir):
+
+def detect_android():
+    from os import environ
+    return 'ANDROID_BOOTLOGO' in environ
+
+
+def _getFolderNamesInDIR(Dir):
     return [f for f in os.listdir(Dir) if os.path.isdir(os.path.join(Dir, f))]
 
 
 def getUsedIdsOnDevice():
-	Raw = getFolderNamesInDIR(Paths["LBTXS"])
-	Refined = []
-	for File in Raw:
-		Refine = File[5:]
-		Refined.append(Refine)
-	return Refined
-	
+    if not os.path.isdir(_Paths["PG3DRoot"]):
+        raise FileNotFoundError(f"Could not find {_Paths['PG3DRoot']}. Is Pixel Gun 3D installed?")
+    if not os.path.isdir(_Paths["LBTXS"]):
+        raise FileNotFoundError(f"Could not find {_Paths['LBTXS']}.")
+    try:
+        return [file[5:] for file in _getFolderNamesInDIR(_Paths["LBTXS"])]
+    except PermissionError:
+        raise PermissionError("Could not access {Paths['LBTXS']}. Is your device rooted?")
+
+
+def _disableupdatechecking():
+    with open(__file__, 'w') as f:
+        new = currentcode.replace("\nupdatechecking = True\n", "\nupdatechecking = False\n")
+        f.write(new)
+
+
+if updatechecking:
+    tocheckforupdates = input("PG3D API: Would you like to check for updates? (Y/N):")
+    if tocheckforupdates.strip().lower() == "y":
+        checkforupdates()
+    else:
+        todisable = input("Would you like to permanently disable update checking? (Y/N):")
+        if todisable.strip().lower() == "y":
+            with open(__file__, 'r') as f:
+                currentcode = f.read()
+            try:
+                _disableupdatechecking()
+            except Exception as err:
+                print("Failed to permanently disable update checking: " + str(err))
+                try:
+                    with open(__file__, 'r+') as f:
+                        if f.read() != currentcode:
+                            print("PG3D API has been corrupted. Attempting to repair...")
+                            try:
+                                f.write(currentcode)
+                            except Exception:
+                                print("Failed to repair PG3D API.")
+                            else:
+                                print("Successfully repaired PG3D API.")
+                except Exception:
+                    pass
+            else:
+                print("Successfully disabled update checking! Rerun PG3D API.")
+                sys.exit()
+
+if not detect_android():
+    raise RuntimeError("PG3D API is only for Android devices!")
+
+if __name__ == "__main__":
+    print(getUsedIdsOnDevice())
